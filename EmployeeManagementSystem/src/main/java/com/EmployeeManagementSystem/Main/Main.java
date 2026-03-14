dd74c6479b4f0dcab34821099da1bbbade4c18ca
Subarna Basak <basaksubarna39@gmail.com>
2026-03-14 23:17:11 +0530

    Remove unnecessary comments and clean up Main class

--- EmployeeManagementSystem/src/main/java/com/EmployeeManagementSystem/Main/Main.java
+++ EmployeeManagementSystem/src/main/java/com/EmployeeManagementSystem/Main/Main.java
@@ -10,21 +10,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Step 1: Load the Spring IoC container
+       
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // Step 2: Get EmployeeService bean from container
         EmployeeService employeeService =
                 (EmployeeService) context.getBean("employeeServiceBean");
 
-        // Step 3: Call all service methods
         employeeService.addEmployee();
         employeeService.getEmployee();
         employeeService.updateEmployee();
         employeeService.deleteEmployee();
 
-        // Step 4: Close the context
         ((ClassPathXmlApplicationContext) context).close();
     }
 }
