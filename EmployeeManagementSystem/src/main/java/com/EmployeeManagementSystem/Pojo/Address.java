0c1f362f4e464340e740a3e52b96327fe7cc5300
Subarna Basak <basaksubarna39@gmail.com>
2026-03-14 23:17:48 +0530

    Refactor Address class formatting and structure

--- EmployeeManagementSystem/src/main/java/com/EmployeeManagementSystem/Pojo/Address.java
+++ EmployeeManagementSystem/src/main/java/com/EmployeeManagementSystem/Pojo/Address.java
@@ -9,7 +9,7 @@ public class Address {
     private String country;
     private int pincode;
 
-    // Constructor Injection - used by Spring XML
+   
     public Address(int id, String city, String state, String country, int pincode) {
         this.id = id;
         this.city = city;
@@ -18,14 +18,13 @@ public class Address {
         this.pincode = pincode;
     }
 
-    // Getters
+
     public int getId()       { return id; }
     public String getCity()  { return city; }
     public String getState() { return state; }
     public String getCountry() { return country; }
     public int getPincode()  { return pincode; }
 
-    // Setters
     public void setId(int id)             { this.id = id; }
     public void setCity(String city)      { this.city = city; }
     public void setState(String state)    { this.state = state; }
